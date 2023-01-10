91a282d0f4e744ceb38697f1a11cd64960b708ae
Sarah Menu <sarah.menu@gmail.com>
2023-01-10 17:35:59 +0100

    added detailed meetup page with react component

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,12 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image='https://images.unsplash.com/photo-1494522855154-9297ac14b55f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+      title="Chicago Meetup"
+      address="20 E Chestnut street, Chicago IL 60611, US" description="This is a first meetup!"/>
+  );
+}
+
+export default MeetupDetails;
