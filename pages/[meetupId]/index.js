7bd33e1c3ed509d5daa254b4ab13f1609a12ca6e
Sarah Menu <sarah.menu@gmail.com>
2023-01-13 16:09:38 +0100

    Connected and saving to db

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -6,7 +6,8 @@ function MeetupDetails() {
     <MeetupDetail
       image='https://images.unsplash.com/photo-1494522855154-9297ac14b55f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
       title="Chicago Meetup"
-      address="20 E Chestnut street, Chicago IL 60611, US" description="This is a first meetup!"/>
+      address="20 E Chestnut street, Chicago IL 60611, US"
+      description="This is a first meetup!"/>
   );
 }
 
